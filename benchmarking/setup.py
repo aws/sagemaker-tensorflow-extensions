acfccf8ef552dff275592a8f7cae391a9481e6a8
Thomas <owenthom@amazon.com>
2018-05-30 16:36:58 -0700

    Add boto3 install_requires to pipemode benchmark

--- benchmarking/setup.py
+++ benchmarking/setup.py
@@ -7,4 +7,5 @@ setup(name="sagemaker_tensorflow_pipemode_benchmarking",
           'console_scripts': ["tensorflow_pipemode_benchmark = pipemode_benchmark.benchmark:main"]
       },
       include_package_data=True,
-      package_data={'pipemode_benchmark': ['docker/*']})
+      package_data={'pipemode_benchmark': ['docker/*']},
+      install_requires=['boto3'])
