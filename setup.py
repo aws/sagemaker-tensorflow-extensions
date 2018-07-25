03bbf7d247bf702432be090f3a4e36068e5533d5
Thomas <owenthom@amazon.com>
2018-07-24 18:24:02 -0700

    Integ test bug fixes

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
     ],
-    install_requires=['cmake', 'tensorflow==1.8'],
+    install_requires=['tensorflow>=1.8,<1.9'],
     extras_require={
         'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock',
                  'sagemaker', 'docker']
