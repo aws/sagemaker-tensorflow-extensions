aab65aac0bfc6882a8ae95fdaa06522f4919be07
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2019-11-21 15:14:24 -0800

    feature: upgrade to TF 1.15

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='sagemaker_tensorflow',
-    version='1.14.0.1.0.0',
+    version='1.15.0.1.0.0',
     description='Amazon Sagemaker specific TensorFlow extensions.',
     packages=find_packages(where='src', exclude=('test',)),
     package_dir={'': 'src'},
