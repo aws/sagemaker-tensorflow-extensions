a8ced2e11b1134fa4e38d9704900b776c9216dc9
Chuyang Deng <chuyangd@amazon.com>
2020-04-27 16:18:10 -0700

    update setup.py programming language from 3.6 to 3.7

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
     extras_require={
         'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock',
