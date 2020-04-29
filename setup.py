0ab5e1425d437ecf07215a30daa024bc9a8f4cb8
Chuyang <chuyangdeng@gmail.com>
2020-04-29 11:51:46 -0700

    feature: python3.7 support (#73)
    * feature: python3.7 support

--- setup.py
+++ setup.py
@@ -115,9 +115,8 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     extras_require={
         'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock',
