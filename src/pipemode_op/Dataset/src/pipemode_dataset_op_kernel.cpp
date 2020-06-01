943f1d985a60947195d6781e41b09b6a5f427015
Ajay Karpur <akarpur@amazon.com>
2020-06-01 12:43:14 -0700

    fix typo

--- src/pipemode_op/Dataset/src/pipemode_dataset_op_kernel.cpp
+++ src/pipemode_op/Dataset/src/pipemode_dataset_op_kernel.cpp
@@ -11,7 +11,7 @@
 // ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-#include "PipeStateManager.pp"
+#include "PipeStateManager.hpp"
 #include "RecordIOReader.hpp"
 #include "TextLineRecordReader.hpp"
 #include "TFRecordReader.hpp"
