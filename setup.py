5568d02f30f9104f7d78a3431311066a832dc1be
Ahsan Khan <ahsan.al.zaki@gmail.com>
2022-01-27 13:02:39 -0800

    Upgrade TensorFlow to 2.7

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='sagemaker_tensorflow',
-    version='2.5.0.1.9.0',
+    version='2.7.0.1.12.0',
     description='Amazon Sagemaker specific TensorFlow extensions.',
     packages=find_packages(where='src', exclude=('test',)),
     package_dir={'': 'src'},
