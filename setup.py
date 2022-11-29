2c6edd95f5c0a7572622a60403f4c042a9effae9
Clayton Parnell <crparnell1998@gmail.com>
2022-11-29 19:30:22 +0000

    Feature: small fixes and major release 1.17.0

--- setup.py
+++ setup.py
@@ -120,6 +120,8 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     extras_require={
         'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock',
